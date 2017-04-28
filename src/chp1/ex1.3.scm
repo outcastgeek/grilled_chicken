2b560d7ea428786b4cab53abb54f0b68511e360b
outcastgeek <outcastgeek+git@gmail.com>
2017-04-28 08:46:53 -0500

    Exercise 1.3 Completion

--- src/chp1/ex1.3.scm
+++ src/chp1/ex1.3.scm
@@ -1,5 +1,31 @@
 ;; Exercise 1.3
 
-;; (define (sum-of-squares-of-two-largest x y z)
-;;   (cond ((and (> x y)) )))
+(define (sum-of-squares-of-two-largest x y z)
+  (cond ((>= x y) (+ (* x x)
+                     (cond ((>= y z) (* y y))
+                           ((> z y) (* z z)))
+                     ))
+        ((> y x) (+ (* y y)
+                    (cond ((>= x z) (* x x))
+                          ((> z x) (* z z)))
+                    ))
+        ))
+
+(sum-of-squares-of-two-largest 1 2 3)
+
+(sum-of-squares-of-two-largest 2 1 3)
+
+(sum-of-squares-of-two-largest 2 3 1)
+
+(sum-of-squares-of-two-largest 3 1 2)
+
+(sum-of-squares-of-two-largest 3 2 1)
+
+(sum-of-squares-of-two-largest 2 3 3)
+
+(sum-of-squares-of-two-largest 3 3 2)
+
+(sum-of-squares-of-two-largest 3 2 3)
+
+(sum-of-squares-of-two-largest 3 3 3)
 
